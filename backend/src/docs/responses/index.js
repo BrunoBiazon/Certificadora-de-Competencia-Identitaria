48806d1b99aaafbbf0adc34e57eb945f17dfaf88
Luís <77413441+Luis-Spessoto@users.noreply.github.com>
2026-06-29 19:38:00 -0300

    Delete backend directory

--- backend/src/docs/responses/index.js
+++
@@ -1,9 +0,0 @@
-const notFound = require("./notFound");
-const badRequest = require("./badRequest");
-const serverError = require("./serverError");
-
-module.exports = {
-  NotFound: notFound,
-  BadRequest: badRequest,
-  ServerError: serverError
-};
--- backend/src/docs/tags/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-  { name: "Alunos", description: "Gerenciamento de alunos participantes" },
-  { name: "Professores", description: "Gerenciamento de professores orientadores" },
-  { name: "Tutores", description: "Gerenciamento de tutores monitores" },
-  { name: "Temas", description: "Gerenciamento de temas das oficinas" },
-  { name: "Oficinas", description: "Gerenciamento de oficinas agendadas" },
-  { name: "Turmas", description: "Gerenciamento de turmas de alunos" }
-];
